944863bb20ae4aaacc51721becfc38e90e9299bb
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-07-06 15:29:31 -0400

    Modified server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/../client'));
 //app.use(express.static(__dirname + '/../styles'));
+var port = process.env.PORT || 9000;
 app.listen(9000, function(err){
   if (err){
     console.log('server sucks');
