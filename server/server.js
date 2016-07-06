9bf86a4fc6b3d4646ff2a7ab992444c6f3707a7e
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-07-06 15:36:34 -0400

    Changed port to variable

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/../client'));
 //app.use(express.static(__dirname + '/../styles'));
 var port = process.env.PORT || 9000;
-app.listen(9000, function(err){
+app.listen(port, function(err){
   if (err){
     console.log('server sucks');
   }
