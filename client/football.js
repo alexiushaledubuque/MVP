c51afd28e4b9615e732f3639870c377681127407
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-07-06 18:09:07 -0400

    Code fixes and Style Changes

--- client/football.js
+++ client/football.js
@@ -1,18 +1,4 @@
-// app2.controller('ctrl1', function($scope){
-//   $scope.randomNum1 = Math.floor((Math.random() * 10) + 1);
-//   $scope.randomNum2 = Math.floor((Math.random() * 10) + 1);
-// });
-
-// app2.controller('badCtrl', function($scope){
-//   var badFeelings = ['Disregarded', 'Unimportant', 'Rejected', 'Powerless'];
-//   $scope.bad = badFeelings[Math.floor((Math.random() * 4))];
-// });
-
-// app2.controller('goodCtrl', function($scope){
-//   var goodFeelings = ['Pleasure', 'Awesome', 'Lovable', 'Inner Peace'];
-//   $scope.good = goodFeelings[Math.floor((Math.random() * 4))]; // indexes change to display different moods
-// });
-
+// need to modularize more for visual clarity!!!
 
 angular.module('mvpApp1', [])    
     .controller('mvpCtrl1', function($scope, $http) {
@@ -38,23 +24,23 @@ angular.module('mvpApp1', [])
         });
       };
 
-      // $scope.getBaseballData = function(){
-      //   $http({
-      //     "method": "GET",
-      //     "url": "https://www.stattleship.com/baseball/mlb/teams",
-      //     "headers": {
-      //      "content-type": "application/json",
-      //      "authorization": "Token token=88e78b68639eefde51d4f67c89e97735",
-      //      "accept": "application/vnd.stattleship.com; version=1",
-      //      "cache-control": "no-cache"
-      //     }
-      //   }).then(function mySucces(response) {
-      //       $scope.bteams = response.data["teams"];
-      //       console.log(response.data);
-      //   }, function error(error) {
-      //       console.log(error);
-      //   });
-      // };
+      $scope.getBaseballData = function(){
+        $http({
+          "method": "GET",
+          "url": "https://www.stattleship.com/baseball/mlb/teams",
+          "headers": {
+           "content-type": "application/json",
+           "authorization": "Token token=88e78b68639eefde51d4f67c89e97735",
+           "accept": "application/vnd.stattleship.com; version=1",
+           "cache-control": "no-cache"
+          }
+        }).then(function mySucces(response) {
+            $scope.bteams = response.data["teams"];
+            console.log(response.data);
+        }, function error(error) {
+            console.log(error);
+        });
+      };
 
       $scope.getBasketballData = function(){
         $http({
