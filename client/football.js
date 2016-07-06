7da9910cd0b86cbfe9de33d8212c9b55654b375d
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-07-06 16:39:24 -0400

    Adding Functionality

--- client/football.js
+++ client/football.js
@@ -1,9 +1,27 @@
+// app2.controller('ctrl1', function($scope){
+//   $scope.randomNum1 = Math.floor((Math.random() * 10) + 1);
+//   $scope.randomNum2 = Math.floor((Math.random() * 10) + 1);
+// });
+
+// app2.controller('badCtrl', function($scope){
+//   var badFeelings = ['Disregarded', 'Unimportant', 'Rejected', 'Powerless'];
+//   $scope.bad = badFeelings[Math.floor((Math.random() * 4))];
+// });
+
+// app2.controller('goodCtrl', function($scope){
+//   var goodFeelings = ['Pleasure', 'Awesome', 'Lovable', 'Inner Peace'];
+//   $scope.good = goodFeelings[Math.floor((Math.random() * 4))]; // indexes change to display different moods
+// });
+
+
 angular.module('mvpApp1', [])    
     .controller('mvpCtrl1', function($scope, $http) {
       $scope.teams = {};
       $scope.bteams = {};
+      $scope.bkteams = {};
+      $scope.hteams = {};
 
-      $scope.getData = function(){
+      $scope.getFootballData = function(){
         $http({
           "method": "GET",
           "url": "https://www.stattleship.com/football/nfl/teams",
@@ -15,18 +33,33 @@ angular.module('mvpApp1', [])
           }
         }).then(function mySucces(response) {
             $scope.teams = response.data["teams"];
-            $scope.league = response.data["league"];;
-            $scope.conference = response.data["conference"];;
-            $scope.divisions = response.data["divisions"];;
         }, function error(error) {
             console.log(error);
         });
       };
 
-      $scope.getBaseballData = function(){
+      // $scope.getBaseballData = function(){
+      //   $http({
+      //     "method": "GET",
+      //     "url": "https://www.stattleship.com/baseball/mlb/teams",
+      //     "headers": {
+      //      "content-type": "application/json",
+      //      "authorization": "Token token=88e78b68639eefde51d4f67c89e97735",
+      //      "accept": "application/vnd.stattleship.com; version=1",
+      //      "cache-control": "no-cache"
+      //     }
+      //   }).then(function mySucces(response) {
+      //       $scope.bteams = response.data["teams"];
+      //       console.log(response.data);
+      //   }, function error(error) {
+      //       console.log(error);
+      //   });
+      // };
+
+      $scope.getBasketballData = function(){
         $http({
           "method": "GET",
-          "url": "https://www.stattleship.com/baseball/mlb/teams",
+          "url": "https://www.stattleship.com/basketball/nba/teams",
           "headers": {
            "content-type": "application/json",
            "authorization": "Token token=88e78b68639eefde51d4f67c89e97735",
@@ -34,10 +67,28 @@ angular.module('mvpApp1', [])
            "cache-control": "no-cache"
           }
         }).then(function mySucces(response) {
-            $scope.bteams = response.data;
+            $scope.bteams = response.data["teams"];
             console.log(response.data);
         }, function error(error) {
             console.log(error);
         });
       };
+
+      // $scope.getHockeyData = function(){
+      //   $http({
+      //     "method": "GET",
+      //     "url": "https://www.stattleship.com/hockey/nhl/teams",
+      //     "headers": {
+      //      "content-type": "application/json",
+      //      "authorization": "Token token=88e78b68639eefde51d4f67c89e97735",
+      //      "accept": "application/vnd.stattleship.com; version=1",
+      //      "cache-control": "no-cache"
+      //     }
+      //   }).then(function mySucces(response) {
+      //       $scope.bteams = response.data["teams"];
+      //       console.log(response.data);
+      //   }, function error(error) {
+      //       console.log(error);
+      //   });
+      // };
 });
--- server/server.js
+++ server/server.js
@@ -4,14 +4,10 @@ var app = express();
 app.use(express.static(__dirname + '/../client'));
 //app.use(express.static(__dirname + '/../styles'));
 var port = process.env.PORT || 9000;
+
 app.listen(port, function(err){
   if (err){
     console.log('server sucks');
   }
   console.log('server is listening.....')
-})
-//var app = require('server.js');
-
-//app.listen(9000);
-// configure our server with all the middleware and and routing
-// require('middleware.js')(app, express);
+});
